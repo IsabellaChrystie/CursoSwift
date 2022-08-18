ccae26f12831904d4c9386840e0214c731bc8b73
LordPinguim <jf.lipeto@gmail.com>
2022-08-18 15:32:46 -0400

    isa

---
+++ IMC/IMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by ICMMAC09-BF7F on 16/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
